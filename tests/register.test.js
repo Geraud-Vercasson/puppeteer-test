776a4ee79aea3160cd39c3d795d7ae67da874dbb
Geraud-Vercasson <geraud.vercasson@gmail.com>
2018-10-26 17:08:51 +0200

    clean up

--- tests/register.test.js
+++ tests/register.test.js
@@ -27,23 +27,18 @@ describe("Register with new credentials", ()=> {
 
         await page.waitForSelector('.toast-message');
         await page.screenshot({path: './tests/img/register2.png'});
+        const html = await page.$eval('.toast-message', _ => _.innerHTML);
+        expect(html).toContain('Thanks for signing up!');
 
     }, timeout);
 
     test(`Delete ${fakeUsername}`, async () => {
-        await page.goto('http://polr.campus-grenoble.fr/login');
-        await page.waitForSelector('div.content-div form [name=username]');
-        await page.type('div.content-div form [name=username]', admin.username);
-        await page.waitForSelector('div.content-div form [name=password]');
-        await page.type('div.content-div form [name=password]', admin.password);
-
-        await page.screenshot({path: './tests/img/delete1.png'});
-
-        await page.waitForSelector('div.content-div form [type=submit]');
-        await page.$eval('div.content-div form [type=submit]', _ => _.click());
+        
+        await login(page, admin.username, admin.password, './tests/img/delete1.png');
+        
         await page.waitForSelector('h1.title');
         await page.screenshot({path: './tests/img/logged.png'});
-
+        
         await page.goto('http://polr.campus-grenoble.fr/admin#admin');
         await page.waitForSelector('#admin_users_table_filter [type=search]');
         await page.screenshot({path: './tests/img/waited_search.png'});
@@ -52,20 +47,39 @@ describe("Register with new credentials", ()=> {
         await page.waitFor(1000);
         await page.waitForSelector('#admin_users_table tr:first-child a.btn-danger');
         await page.screenshot({path: './tests/img/delete3.png'});
-
+        
         await page.$eval( '#admin_users_table tr:first-child a.btn-danger', _ => _.click() );
         await page.waitForSelector('.toast-message');
         await page.screenshot({path: './tests/img/delete4.png'});
-
+        const html = await page.$eval('.toast-message', _ => _.innerHTML);
+        expect(html).toContain('User successfully deleted');
+        
+        //tring to reconnect
+        await login(page, fakeUsername, fakePassword, './tests/img/reconnect1.png');
+        await page.waitForSelector('.toast-message');
+        await page.screenshot({path: './tests/img/reconnect2.png'});
+        const html2 = await page.$eval('.toast-message', _ => _.innerHTML);
+        expect(html2).toContain('Invalid password or inactivated account. Try again.');
     }, timeout);
     // creation of a browsable page
     beforeAll( async () => {
         page = await global.__BROWSER__.newPage();
-
+        
         page.on('dialog', async dialog => {
             await dialog.accept();
         });
     }, timeout);
-
+    
 });
 
+async function login(page, username, pass, screenshotPath) {
+    await page.goto('http://polr.campus-grenoble.fr/login');
+    await page.waitForSelector('div.content-div form [name=username]');
+    await page.type('div.content-div form [name=username]', username);
+    await page.waitForSelector('div.content-div form [name=password]');
+    await page.type('div.content-div form [name=password]', pass);
+    await page.screenshot({path: screenshotPath});
+    await page.waitForSelector('div.content-div form [type=submit]');
+    await page.$eval('div.content-div form [type=submit]', _ => _.click());    
+    
+}
